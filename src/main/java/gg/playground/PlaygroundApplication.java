4097cc422ca03788056ff1393c2ab0e625b4d4eb
TwistedDNA <maksym.dna.mazur@gmail.com>
2017-02-21 20:48:45 +0100

    Changed application type to web server

--- src/main/java/gg/playground/PlaygroundApplication.java
+++ src/main/java/gg/playground/PlaygroundApplication.java
@@ -1,20 +1,14 @@
 package gg.playground;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PlaygroundApplication implements CommandLineRunner {
+public class PlaygroundApplication {
 
 
     public static void main(String[] args) {
         SpringApplication.run(PlaygroundApplication.class, args);
-    }
-
-    @Override
-    public void run(String... args) {
-
     }
 
 }
